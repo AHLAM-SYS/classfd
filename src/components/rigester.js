41af1ce1b8d6c4a9b07ebb3d9393c886982987fc
yuit <fjgf@gmail.com>
2024-11-01 18:20:21 +0300

    fix

--- src/components/rigester.js
+++ src/components/rigester.js
@@ -7,7 +7,7 @@ function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post('http://localhost:5000/register', { username, password });
+    await axios.post('https://classback-3.onrender.com/register', { username, password });
     alert('User registered successfully');
   };
 
