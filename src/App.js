78a480c4e8186cdbdab9c3ec00a39ad2bb7664c5
yuit <fjgf@gmail.com>
2024-11-01 16:46:56 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
